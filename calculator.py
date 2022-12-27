888ea0f8ca81ed1232c1892dec59a489b81d5f0b
KENNETH COX <kennethcox@KENNETHs-MacBook-Pro.local>
2022-12-27 13:03:59 -0600

    Add unit test for calculator

---
+++ calculator.py
@@ -0,0 +1,9 @@
+"""
+Calculator library containing basic math operations.
+"""
+
+def add(first_item, second_item):
+    return first_item + second_item
+
+def subtract(first_item, second_item):
+    return first_item - second_item
